224d91c322ec6315693b4aa74b53a9786c7c8376
AnwitDahal <dahalanwit0707@gmail.com>
2024-08-20 10:25:21 +0545

    Frontend Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import FloatingShape from "./components/FloatingShape";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+      <FloatingShape
+        color="bg-green-500"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-green-500"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-green-500"
+        size="w-32 h-32"
+        top="40%"
+        left="10%"
+        delay={2}
+      />
+
+      <Routes>
+        <Route path='/' element={"Home"}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
