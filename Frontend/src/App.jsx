6beb806cd73b5985e1a4e4b9a03d7e668b588ada
AnwitDahal <dahalanwit0707@gmail.com>
2024-08-21 21:10:32 +0545

    SignUp Page UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,23 +9,23 @@ import Login from "./Pages/Login";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-800 to-sky-900 flex items-center justify-center relative overflow-hidden">
       <FloatingShape
-        color="bg-green-500"
+        color="bg-blue-500"
         size="w-64 h-64"
         top="-5%"
         left="10%"
         delay={0}
       />
       <FloatingShape
-        color="bg-green-500"
+        color="bg-blue-500"
         size="w-48 h-48"
         top="70%"
         left="80%"
         delay={5}
       />
       <FloatingShape
-        color="bg-green-500"
+        color="bg-blue-500"
         size="w-32 h-32"
         top="40%"
         left="10%"
@@ -33,9 +33,9 @@ function App() {
       />
 
       <Routes>
-        <Route path='/' element={"Home"}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path="/" element={"Home"} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
