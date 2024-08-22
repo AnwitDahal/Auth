b5d021c00351a88a2505fdb68703602791f801ab
AnwitDahal <dahalanwit0707@gmail.com>
2024-08-23 00:46:35 +0545

    Implementing SignUp

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cookieParser=require('cookie-parser')
 require("dotenv").config();
+const cors=require('cors')
 const { connectDB } = require("./database/connectDB");
 const authRoutes = require("./routes/auth");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({origin:"http://localhost:5173", credentials:true}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- Frontend/src/Pages/SignUp.jsx
+++ Frontend/src/Pages/SignUp.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Input from "../components/Input";
-import { Lock, Mail, User } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Loader, Lock, Mail, Trophy, User } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordStrength from "../components/PasswordStrength";
+import { useAuthStore } from "../AuthStore";
 
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate=useNavigate();
 
-  const handleSignUp = (e) => {
+  const {signup,error,isLoading}=useAuthStore();
+
+  const handleSignUp = async (e) => {
     e.preventDefault();
+
+    try{
+      await signup(email,password,name);
+      navigate('/verify-email')
+    } 
+    catch(err){
+      console.log(err);
+      
+    }
   };
   return (
     <motion.div
@@ -50,6 +63,7 @@ const SignUp = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
           {/* Password Strength Tester */}
+          {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
           <PasswordStrength password={password} />
           <motion.button
             className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-blue-500 to-sky-600 text-white 
@@ -59,9 +73,9 @@ const SignUp = () => {
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
             type="submit"
-            // disabled={isLoading}
+            disabled={isLoading}
           >
-            Sign Up
+            {isLoading?<Loader className="animate-spin mx-auto" size={24}/>:"Sign Up"}
           </motion.button>
         </form>
       </div>
